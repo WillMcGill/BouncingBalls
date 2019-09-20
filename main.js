b31cf3457883ccddfb63dbd4ec650403bbecd1ee
Will McGIll <willmcgill@Makiyas-iPhone.localdomain>
2019-09-20 15:18:09 -0400

    created child of Shape

--- main.js
+++ main.js
@@ -14,15 +14,23 @@ function random(min,max) {
 
   
   }
-  function Ball(x, y, velX, velY, color, size) {
+  function Shape(x, y, velX, velY, exists) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
-    this.color = color;
-    this.size = size;
+    this.exists = true;
+    
   
 }
+
+function Ball (x, y, velX, velY, color, size, exists){        //Create Ball constructor based Shape constructor
+  Shape.call(this, x, y, velX, velY, exists)
+
+  this.color = color;
+  this.size = size;
+}
+
 
 Ball.prototype.draw = function() {
     ctx.beginPath();
@@ -70,6 +78,20 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+Ball.prototype.collisionDetect = function() {
+    for (var j = 0; j < balls.length; j++) {
+      if (!(this === balls[j])) {
+        var dx = this.x - balls[j].x;
+        var dy = this.y - balls[j].y;
+        var distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + balls[j].size) {
+          balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+        }
+      }
+    }
+  }
+
 function loop() {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
     ctx.fillRect(0, 0, width, height);
@@ -77,9 +99,14 @@ function loop() {
     for (var i = 0; i < balls.length; i++) {
       balls[i].draw();
       balls[i].update();
+      balls[i].collisionDetect();
     }
   
     requestAnimationFrame(loop);
   }
 
   loop();
+
+
+
+  

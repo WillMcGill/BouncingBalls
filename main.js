73c51ba9d749e8e035713ee626c23fc3cccd7e30
Will McGIll <willmcgill@Makiyas-iPhone.localdomain>
2019-09-23 11:19:02 -0400

    added evil ball

--- main.js
+++ main.js
@@ -32,11 +32,79 @@ function Ball(x, y, velX, velY, exists, color, size) {
   this.size = size;
 }
 
+
+
 Ball.prototype = Object.create(Shape.prototype);
 Ball.prototype.constructor = Ball;
 
+//created Evil Circle, properties inherited from Shape()
+
+function EvilCircle(x, y, velX, velY, exists, color, size) {
+  Shape.call(this, x, y, 20, 20, exists);
+
+  this.color = "rgb(255, 255, 255)";
+  this.size = 10;
+}
+
+//Evil Circle draw function, updated to not fill
+EvilCircle.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.stroke();
+};
+
+EvilCircle.prototype.update = function() {
+  if((this.x + this.size) >= width) {
+    this.x = (this.size);
+  }
+
+  if((this.x - this.size) <= 0) {
+    this.x = (this.size);
+  }
+
+  if((this.y + this.size) >= height) {
+    this.y = (this.size);
+  }
+
+  if((this.y - this.size) <= 0) {
+    this.y = (this.size);
+  }
+};
+
+EvilCircle.prototype.setControls = function(){
+  var _this = this;
+  window.onkeydown = function(e) {
+      if (e.keyCode === 37) {
+        _this.x -= _this.velX;
+      } else if (e.keyCode === 39) {
+        _this.x += _this.velX;
+      } else if (e.keyCode === 38) {
+        _this.y -= _this.velY;
+      } else if (e.keyCode === 40) {
+        _this.y += _this.velY;
+      }
+    }
+}
+
+EvilCircle.prototype.collisionDetect = function() {
+  for(var j = 0; j < balls.length; j++) {
+    if(!(balls[j].exists === true)) {
+      var dx = this.x - balls[j].x;
+      var dy = this.y - balls[j].y;
+      var distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].exists = false;
+      }
+    }
+  }
+};
+
 Ball.prototype.draw = function() {
   ctx.beginPath();
+  //ctx.lineWidth = 3;
   ctx.fillStyle = this.color;
   ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
   ctx.fill();
@@ -88,7 +156,7 @@ while(balls.length < 25) {
   var size = random(10,20);
   var ball = new Ball(
     // ball position always drawn at least one ball width
-    // away from the adge of the canvas, to avoid drawing errors
+    // away from the edge of the canvas, to avoid drawing errors
     random(0 + size,width - size),
     random(0 + size,height - size),
     random(-7,7),
@@ -102,15 +170,23 @@ while(balls.length < 25) {
 
 // define loop that keeps drawing the scene constantly
 
+var evilCircle = new EvilCircle(10, 10, true);
+evilCircle.setControls();
+
 function loop() {
   ctx.fillStyle = 'rgba(0,0,0,0.25)';
   ctx.fillRect(0,0,width,height);
-
+  
   for(var i = 0; i < balls.length; i++) {
     balls[i].draw();
     balls[i].update();
     balls[i].collisionDetect();
+    
+    
   }
+    evilCircle.draw();
+    evilCircle.update();
+    evilCircle.collisionDetect();
 
   requestAnimationFrame(loop);
 }
